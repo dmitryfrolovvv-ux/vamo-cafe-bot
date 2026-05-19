53eb64b6172159e88d3b6b9644b4beaeee8158de
dmitryfrolovvv-ux <dmitry.frolovvv@gmail.com>
2026-05-20 02:05:06 +0300

    Fix Render port

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.types import Message
+from aiohttp import web
 import asyncio
 import os
 
@@ -27,8 +28,24 @@ async def start_handler(message: Message):
     )
 
 
+async def healthcheck(request):
+    return web.Response(text="VAMO Cafe Bot is running")
+
+
 async def main():
     print("VAMO Cafe Bot started")
+
+    app = web.Application()
+    app.router.add_get("/", healthcheck)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    port = int(os.environ.get("PORT", 10000))
+
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+
     await dp.start_polling(bot)
 
 
