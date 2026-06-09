8162598cada68af8a4c77492557cc8a826cef6f8
dmitryfrolovvv-ux <dmitry.frolovvv@gmail.com>
2026-06-09 18:33:56 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -163,9 +163,9 @@ def register_admin(dp, conn, cur, main_menu, is_admin):
         lambda m: m.text == "📢 Post to channel",
         state="*"
     )
-async def channel_post_start(
-    message: types.Message
-):
+    async def channel_post_start(
+        message: types.Message
+    ):
 
         if not is_admin(message.from_user.id):
             return
@@ -183,7 +183,7 @@ async def channel_post_start(
             reply_markup=kb
         )
     
-    await ChannelPost.photo.set()
+        await ChannelPost.photo.set()
     
     @dp.message_handler(
         lambda m: m.text == "📦 Product editor",
